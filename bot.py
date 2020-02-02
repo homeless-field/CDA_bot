36c346dfec11ebb4ed0871eb9409bb1c94f84e2e
homeless-field <59347006+homeless-field@users.noreply.github.com>
2020-02-01 20:57:51 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,48 @@
+from time import sleep
 from requests import get
 from bs4 import BeautifulSoup
-from datetime import date
+from datetime import datetime
+import praw
 
-# Get the headline article from the website of local news sources
-cdaPress = str(BeautifulSoup(get('https://www.cdapress.com/news/local-news').text, features='html.parser').find('div', class_='headlines-section').h1)
-spokesmanReview = str(BeautifulSoup(get('https://www.spokesman.com/idaho').text, features='html.parser').find('h1', class_='lh-solid f4 f3-l mt0 mb1 sans-serif fw8 f2-l nt1-l').text)
-krem2 = str(BeautifulSoup(get('https://www.krem.com/section/news/local/kootenai-county').text, features='html.parser').find('div', class_='story-snapshot-with-abstract__mobile-title dot-ellipsis dot-height-135').text)
-khq = str(BeautifulSoup(get('https://www.khq.com/news').text, features='html.parser').find('div', class_='card-headline').text)
+# Information about the bot and its account
+reddit = praw.Reddit(user_agent='CDA_bot (by /u/QLZX)', client_id='*', client_secret='*', username='CDA_bot', password='*')
 
-# Get the 'read more' links of the headlines
-cdaPressLink = str(BeautifulSoup(get('https://www.cdapress.com/news/local-news').text, features='html.parser').find('p', class_='read-more').a)
-spokesmanReviewLink = str(BeautifulSoup(get('https://www.spokesman.com/idaho').text, features='html.parser').find('h1', class_='lh-solid f4 f3-l mt0 mb1 sans-serif fw8 f2-l nt1-l').a)
-krem2Link = str(BeautifulSoup(get('https://www.krem.com/section/news/local/kootenai-county').text, features='html.parser').find('div', class_='story-snapshot-with-abstract__headline').a)
-khqLink = str(BeautifulSoup(get('https://www.khq.com/news').text, features='html.parser').find('div', class_='card-headline').a)
+while True:
+    # Get the website of local news sources
+    cdaPress = BeautifulSoup(get('https://www.cdapress.com/news/local-news').text, features='html.parser')
+    spokesmanReview = BeautifulSoup(get('https://www.spokesman.com/idaho').text, features='html.parser')
+    krem2 = BeautifulSoup(get('https://www.krem.com/section/news/local/kootenai-county').text, features='html.parser')
+    khq = BeautifulSoup(get('https://www.khq.com/news').text, features='html.parser')
 
-# Print the title
-print("Today's News (" + date.today().strftime('%a, %x') + ")")
-print()
-# Print the headlines and the link to the article
-print("CDA Press: " + cdaPress[4:-5] + "(https://www.cdapress.com" + cdaPressLink[9:-15] + ")")
-print("Spokesman-Review: " + spokesmanReview + "(https://www.spokesman.com" + spokesmanReviewLink[42:-(len(spokesmanReview) + 6)] + ")")
-print("KREM2: " + krem2[26:-22] + "(https://www.krem.com" + krem2Link[117:-((len(krem2) * 2) + 190)] + ")")
-print("KHQ: " + khq[33:] + "(https://www.khq.com/news" + khqLink[32:-(len(khq) + 5)] + ")")
-print()
-# Print the "watermark"
-print("Disclaimer: News is rarely 100% accurate to reality")
-print("I am a bot. To report a bug or make a suggestion, make a comment that includes 'u/QLZX' anywhere in it. View my source code here")
-print("Version 1.0, Timestamp: " + date.today().strftime('%a, %x, %H:%M:%S'))
+    # Get a bulletin headline from each news source
+    cdaPressHeadline = str(cdaPress.find('div', class_='headlines-section').h1)
+    spokesmanReviewHeadline = str(spokesmanReview.find('h1', class_='lh-solid f4 f3-l mt0 mb1 sans-serif fw8 f2-l nt1-l').text)
+    krem2Headline = str(krem2.find('div', class_='story-snapshot-with-abstract__mobile-title dot-ellipsis dot-height-135').text)
+    khqHeadline = str(khq.find('div', class_='card-headline').text)
+
+    # Get a link to the bulletin articles
+    cdaPressLink = str(cdaPress.find('p', class_='read-more').a)
+    spokesmanReviewLink = str(spokesmanReview.find('h1', class_='lh-solid f4 f3-l mt0 mb1 sans-serif fw8 f2-l nt1-l').a)
+    krem2Link = str(krem2.find('div', class_='story-snapshot-with-abstract__headline').a)
+    khqLink = str(khq.find('div', class_='card-headline').a)
+
+    # Set the title of the post
+    title = "Today's News (" + datetime.now().strftime('%a, %x') + ")"
+
+    # Set the body text of the post
+    text = "CDA Press: [" + cdaPressHeadline[4:-5] + "](https://www.cdapress.com" + cdaPressLink[9:-15] + ")\n\n"
+    text += "Spokesman-Review: [" + spokesmanReviewHeadline + "](https://www.spokesman.com" + spokesmanReviewLink[42:-(len(spokesmanReviewHeadline) + 6)] + ")\n\n"
+    text += "KREM2: [" + krem2Headline[26:-22] + "](https://www.krem.com" + krem2Link[117:-((len(krem2Headline) * 2) + 190)] + ")\n\n"
+    text += "KHQ: [" + khqHeadline[33:] + "](https://www.khq.com/news" + khqLink[32:-(len(khqHeadline) + 5)] + ")\n\n"
+
+    # Add a "disclaimer" to the post
+    text += "^^I ^^am ^^a ^^bot. ^^To ^^report ^^a ^^bug ^^or ^^make ^^a ^^suggestion, ^^just ^^make ^^a ^^comment. ^^View ^^my ^^source ^^code ^^[here](https://github.com/homeless-field/CDA_bot)\n\n"
+    text += "^^Version ^^1.0, ^^Timestamp: " + datetime.now().strftime('^^%x, ^^%H:%M:%S')
+
+    # Remove any non-ascii characters from the headlines
+    text = ''.join((c for c in text if 0 < ord(c) < 127))
+
+    # Make the post in r/coeurdalene
+    subreddit = reddit.subreddit('coeurdalene')
+    subreddit.submit(title, text)
+    sleep(86400)
